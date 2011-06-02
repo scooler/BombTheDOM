175d0c11cfd751c600b9a0a1e9858110b21177ce
Pawel Lorek <plorek@neubloc.com>
2011-06-02 21:58:37 +0200

    Partial connecting the io handling, and upgrading animation

--- javascripts/class/player_movement.js
+++ javascripts/class/player_movement.js
@@ -1,32 +1,61 @@
 MyApp.playerMovement = function(elem){  
   var moving = false;
+  var animator;
+
+  var settings = {
+    left: {
+      spritePos: "-144px"
+    },
+    up: {
+      spritePos: "0px"
+    },
+    right: {
+      spritePos: "-48px"
+    },
+    down: {
+      spritePos: "-96px"
+    }
+  };
+
+  var startMovement = function(direction){
+    var movementSettings = settings[direction];
+    var elemPosition = elem.style.backgroundPosition || "0px 0px";
+    elem.style.backgroundPosition = elemPosition.split(" ")[0]+" "+movementSettings.spritePos;
+  };
+
   
   return {
-    //All these functions are togle - you call it once on keyd down
+    //All these functions are togle - you call it once on key down
     moveDown: function(){
-       //TODO   
-       console.log(elem);
-       console.log("moveDown()");
+      startMovement("down");
+      //TODO   
+      console.log(elem);
+      console.log("moveDown()");
     },
     moveUp: function(){
-       //TODO   
-       console.log(elem);
-       console.log("moveUp()");
+      startMovement("up");
+      //TODO   
+      console.log(elem);
+      console.log("moveUp()");
     },
     moveRight: function(){
-       //TODO   
-       console.log(elem);
-       console.log("moveRight()");
+      startMovement("right");
+      //TODO   
+      console.log(elem);
+      console.log("moveRight()");
     },
     moveLeft: function(){
-       //TODO   
-       console.log(elem);
-       console.log("moveLeft()");
+      startMovement("left");
+      //TODO   
+      console.log(elem);
+      console.log("moveLeft()");
     },
     stop: function(){
-       //TODO   
-       console.log(elem);
-       console.log("stop()");
+      if (typeof animator === "object"){
+        animator.stopAnimation();
+      }
+      console.log(elem);
+      console.log("stop()");
     }
   }
 }
--- javascripts/utils_animate.js
+++ javascripts/utils_animate.js
@@ -3,34 +3,32 @@
     MyApp.utils.animate = function(elem, steps, cyclic){
     var countDown = steps;
     var animationSpeed = MyApp.params.animationSpeed;
+    var timerHandle, animationStoped = false;
     var innerAnimate = function(){
+      if (animationStoped){ return };
       var bgPossition = elem.style.backgroundPosition || "0px 0px";
       var currentPossition = parseInt(bgPossition.split(" ")[0]);
       currentPossition = currentPossition - 32;
-      // if (currentStep === steps){
-      //   elem.className = "";
-      //   if (typeof callback === "function"){
-      //     callback(elem);
-      //   }
-      //   return;
-      // }
-
 
       elem.style.backgroundPosition = currentPossition + "px " + bgPossition.split(" ")[1];
       countDown --;
       if (countDown !== 0){
-        setTimeout(innerAnimate, animationSpeed);
+        timerHandle = setTimeout(innerAnimate, animationSpeed);
       }else{
         if (cyclic){ //restart animation
-          elem.style.backgroundPosition = "0px 0px"//currentPossition + "px " + bgPossition.split(" ")[1];
+          elem.style.backgroundPosition = "0px 0px";
           countDown = steps;
-          setTimeout(innerAnimate, animationSpeed);
+          timerHandle = setTimeout(innerAnimate, animationSpeed);
         }
       }
     };
     innerAnimate();
-
-    //TODO return token to stop animation
-  };
+    return {
+      stopAnimation: function(){
+        clearTimeout(timerHandle);
+        animationStoped = true;
+      }
+    }
+  }
 
 })();
